a4f57d8d56eae8bc440c5ad45fe6d690e6464945
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-02 16:52:27 +0100

    prima parte ok

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
         slidePrev();
     });
 
-});
-function slideNext() {
-    $('.next').click(function(){
+    var clock = setInterval(function(){
+        slideNext();
+    }, 2000)
+
+    function slideNext(){
         if ($('.images img.active').hasClass('last')) {
             $('.images img.active').removeClass('active');
             $('.images img.first').addClass('active');
@@ -18,23 +20,21 @@ function slideNext() {
 
             imgAttiva.removeClass('active');
             prossimaImg.addClass('active');
-
         }
-});
-function slidePrev() {
-    if ($('.images img.active').hasClass('first')) {
-        $('.images img.active').removeClass('active');
-        $('.images img.last').addClass('active')
-    } else {
-        var imgAttiva = $('.images img.active');
-        var previousImg = imgAttiva.prev();
+    };
 
-        imgAttiva.removeClass('active');
-        previousImg.addClass('active');
-    }
-});
+    function slidePrev () {
+        if ($('.images img.active').hasClass('first')) {
+            $('.images img.active').removeClass('active');
+            $('.images img.last').addClass('active')
+        } else {
+            var imgAttiva = $('.images img.active');
+            var previousImg = imgAttiva.prev();
 
-
+            imgAttiva.removeClass('active');
+            previousImg.addClass('active');
+        }
+    };
 
 
 
